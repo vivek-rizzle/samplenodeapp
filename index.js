4f0413fb54e694215d0bc4e0fb27ec21a5a2a202
Vivek Kushwah <vivek.k@thsilverlabs.com>
2023-10-03 17:59:20 +0530

    fmt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ setInterval(() => {
 
   console.log(value);
   if(value === 'true' || JSON.parse(value) === true){
-    console.log('LATEST_CODE not in repo ,Exiting the process....Server will be restarted automatically');
+    console.log('LATEST_CODE not in repo , Exiting the process....Server will be restarted automatically');
     const { spawn } = require('child_process');
     const scriptProcess = spawn('bash', ['restart.sh'], { detached: true, stdio: 'ignore' });
     console.log('Triggering the bash script...');
